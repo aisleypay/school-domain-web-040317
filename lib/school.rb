196aa005b65962bbbdada3366c074a953d390ffc
aisleypay <paisley.singh@gmail.com>
2017-04-07 00:15:17 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require "pry"
+class School
+  attr_accessor :name, :roster
+
+  def initialize (name)
+    @name = name
+    @roster = {}
+    #K - grade levels V - array of students
+  end
+
+  def add_student (student_name, grade)
+    @roster[grade] ||= [] 
+    @roster[grade] << student_name
+  end
+
+  def grade (grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each { |grade, students|
+      students.sort!
+    }
+  end
+end
